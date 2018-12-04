1fe5d7904a6f380034822731c995f0548b1ab2b4
ssirawan <45140043+ssirawan@users.noreply.github.com>
2018-12-04 13:09:38 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,4 +1,5 @@
-<?php$access_token = 'uGf6Ts7gXkqHiGr/Po4FwcY6RR0eBSgOm2gfzoq5lx0l4JN2B1nOt/liGAVBba3BPGomLXbzjZOWaK7MQjJsJ3c0kPBhnDo2vxEdES6a2Knm56TyLscmr/gn9Etn7ur+K9c3/1cVgLBWkjldvKtSXAdB04t89/1O/w1cDnyilFU=';
+<?php
+$access_token = 'uGf6Ts7gXkqHiGr/Po4FwcY6RR0eBSgOm2gfzoq5lx0l4JN2B1nOt/liGAVBba3BPGomLXbzjZOWaK7MQjJsJ3c0kPBhnDo2vxEdES6a2Knm56TyLscmr/gn9Etn7ur+K9c3/1cVgLBWkjldvKtSXAdB04t89/1O/w1cDnyilFU=';
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -7,3 +8,4 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 echo $result;
+?>
