6dc37a4a03e0854aaf1bff04433f7ae8aa6fedbe
ssirawan <45140043+ssirawan@users.noreply.github.com>
2018-12-05 15:24:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,56 +1,74 @@
 <?php
-
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'sRsLuKvN5gmQkMfB+Ns7jf8jKlvsmFF90qeMFosqoDySR2QLhKdJfGb59CouxvCNuyxnEBPRfI+W6LjLKzctQU+cJwCLJAFKZ0e8+iwqJJDQHZLRhutLxxNTtGv0MISe0BNnrLBwO7QvXBUKkQP8PgdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'vEcA9SC+uVHF+zBZZQod5Yp/fS2Xn+lUkqHKi1EE1OGXZjtGJlfwrKfkLFu+wOyVPGomLXbzjZOWaK7MQjJsJ3c0kPBhnDo2vxEdES6a2Kk8PnQNwJRLHbPslhqvzC1xk8lM8HLtnERPSG8oXBLNvwdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
-
 if ( sizeof($request_array['events']) > 0 )
 {
-
-		foreach ($request_array['events'] as $event)
-		{
-			$reply_message = '';
-			$reply_token = $event['replyToken'];
-
-			if ( $event['type'] == 'message' ) 
-			{
-				if( $event['message']['type'] == 'text' )
-				{
-					$text = $event['message']['text'];
-					if ($text = 'hi')
-					{
-						$reply_message = 'hi';
-					}
-					
-					else $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
-				}
-				else
-					$reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
-	  
-			}
-			else
-				$reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
- 
-	if( strlen($reply_message) > 0 )
+ foreach ($request_array['events'] as $event)
+ {
+  $reply_message = '';
+  $reply_token = $event['replyToken'];
+  if ( $event['type'] == 'message' ) 
+  {
+   if( $event['message']['type'] == 'text' )
+   {
+    $text = $event['message']['text'];
+	$car = array("'1'","1","1","ค้นหารถ","รถ","หารถ","ยี่ห้อรถ");
+	$tel = array("tel","เบอร์","หาเบอร์","2","'2'","โทร","เบอร์โทร");
+	$check = 0;
+	
+	foreach ($car as $value)
 	{
-		//$reply_message = iconv("tis-620","utf-8",$reply_message);
-		$data = [
-					'replyToken' => $reply_token,
-					'messages' => [['type' => 'text', 'text' => $reply_message]]
-				];
-		$post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-		$send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
-		echo "Result: ".$send_result."\r\n";
-	}
+		if($text == $value)
+		{
+			$check=1;
 		}
+	}
+	foreach ($tel as $value)
+	{
+		if($text == $value)
+		{
+			$check=2;
+		}
+	}
+	
+	if($check ==1)
+	{
+		$reply_message = 'ตอนนี้มีรถอยู่ในระบบจำนวน 20 คัน';
+	}
+	elseif($check ==2)
+	{
+		$reply_message = 'อู่คุณวิชัย 023334444';
+	}
+		
+	else
+		$reply_message = 'พิมพ์ "1" เมื่อต้องการค้นหารถ 
+						   พิมพ์ "2" เมื่อต้องการค้นหาเบอร์ติดต่อของบริษัท
+						   พิมพ์ "3" เมื่อต้องการตรวจสอบการเงิน';
+   }
+   else
+    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+  
+  }
+  else
+   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+ 
+  if( strlen($reply_message) > 0 )
+  {
+   //$reply_message = iconv("tis-620","utf-8",$reply_message);
+   $data = [
+    'replyToken' => $reply_token,
+    'messages' => [['type' => 'text', 'text' => $reply_message]]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+   echo "Result: ".$send_result."\r\n";
+  }
+ }
 }
-
 echo "OK";
-
 function send_reply_message($url, $post_header, $post_body)
 {
  $ch = curl_init($url);
@@ -61,8 +79,6 @@ function send_reply_message($url, $post_header, $post_body)
  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
  $result = curl_exec($ch);
  curl_close($ch);
-
  return $result;
 }
-
 ?>
