dead805eb9f7e914800b4ec14bda0e30b0859823
ssirawan <45140043+ssirawan@users.noreply.github.com>
2018-12-07 17:44:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ mysqli_query($db,"SET NAMES 'utf8' COLLATE 'utf8_general_ci';");
 
 */
 
-
+$dbconn3 = pg_connect("host=ec2-54-235-133-42.compute-1.amazonaws.com port=5432 dbname=dd9n7qflnjtae8 user=ynezixirsjupio password=8aa84c7e317c93709b62657ac0a26d2f8696df1eaee0f6bb108e6cd3a2ca4d22");
 
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
